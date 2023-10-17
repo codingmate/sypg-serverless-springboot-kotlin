3cdb54bfa2b7b153fc8aabecd26b4494294e062d
codingmate <hsykys0728@gmail.com>
2023-10-17 21:21:05 +0900

    231017_ yml프로퍼티) app.name: 값을 설정해야 값을 가져올 수 있네. 왜 그런건진 아직 잘 모르겠다.

--- src/test/kotlin/dev/sypg/learning/properties/PropertiesTest.kt
+++ src/test/kotlin/dev/sypg/learning/properties/PropertiesTest.kt
@@ -7,20 +7,20 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.TestPropertySource
 
+@TestPropertySource( locations = ["/test-application.yml"])
 @SpringBootTest(classes = [Application::class])
-@TestPropertySource(locations = ["/test-application.yml"])
 class PropertiesTest {
 
     @Value("\${app.name}")
     lateinit var appName: String
 
-    @Value("\${app.version}")
-    lateinit var appVersion: String
+//    @Value("\${app.version}")
+//    lateinit var appVersion: String
 
     @Test
     fun `should load properties from test application yml`() {
         assertThat(1).isEqualTo(1)
         assertThat(appName).isEqualTo("sypg")
-        assertThat(appVersion).isEqualTo("1.0.0")
+//        assertThat(appVersion).isEqualTo("1.0.0")
     }
 }
