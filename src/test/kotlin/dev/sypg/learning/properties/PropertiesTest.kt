1cff162ba3d3a10acf8208489362b2e3504ce5ec
codingmate <hsykys0728@gmail.com>
2023-10-17 22:13:25 +0900

    231017_ 프로퍼티를 테스트 할 때, @TestPropertySource()를 사용하면 nested property는 작동하지 않고, flatten property를 사용해야한다.. 왜 그런지는 모르겠다.

--- src/test/kotlin/dev/sypg/learning/properties/PropertiesTest.kt
+++ src/test/kotlin/dev/sypg/learning/properties/PropertiesTest.kt
@@ -7,20 +7,19 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.TestPropertySource
 
-@TestPropertySource( locations = ["/test-application.yml"])
+@TestPropertySource( locations = ["/test-application.yml"] )
 @SpringBootTest(classes = [Application::class])
 class PropertiesTest {
 
     @Value("\${app.name}")
     lateinit var appName: String
 
-//    @Value("\${app.version}")
-//    lateinit var appVersion: String
-
+    @Value("\${app.version}")
+    lateinit var appVersion: String
     @Test
     fun `should load properties from test application yml`() {
         assertThat(1).isEqualTo(1)
         assertThat(appName).isEqualTo("sypg")
-//        assertThat(appVersion).isEqualTo("1.0.0")
+        assertThat(appVersion).isEqualTo("1.0.0")
     }
 }
